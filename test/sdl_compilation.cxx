97f999367cbcb51c57fd9f3dc0eaed95229faaa0
Eugene <glebajk@gmail.com>
2023-04-10 23:42:46 +0300

    Dll injection cmake option fix (Default state is OFF)

--- test/sdl_compilation.cxx
+++ test/sdl_compilation.cxx
@@ -61,7 +61,7 @@ TEST(SDLTest, SDLInitialization) {
     SDL_Quit();
 }
 
-int main(int argc, char** argv) {
+auto main(int argc, char** argv) -> int {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
